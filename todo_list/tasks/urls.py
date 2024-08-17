f60ec54939c6dbaea09bf0d3b7dce53b73799e22
nameisalfio <alfio.spoto@outlook.it>
2024-08-17 16:53:44 +0200

    Have to add CSS

--- todo_list/tasks/urls.py
+++ todo_list/tasks/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from . import views as task_views
 from . models import Task
-from . views import TaskListView, TaskDetailView
+from . views import *
 
 # Task list
 # Single task of list
 
 urlpatterns = [
-    path('', TaskListView.as_view(), name='tasks_list'),
-    path('<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
+    path('', task_views.TaskListView.as_view(), name='tasks_list'),
+    path('welcome/', task_views.welcome, name='welcome'),
     path('about/', task_views.about, name='about'),
+    path('create/', task_views.TaskCreateView.as_view(), name='task_create'),
+    path('<int:pk>/', task_views.TaskDetailView.as_view(), name='task_detail'),
+    path('<int:pk>/update/', task_views.TaskUpdateView.as_view(), name='task_update'),
+    path('<int:pk>/delete/', task_views.TaskDeleteView.as_view(), name='task_delete'),
 ]
--- todo_list/tasks/views.py
+++ todo_list/tasks/views.py
@@ -1,24 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from datetime import datetime
+from django.urls import reverse_lazy
 from .models import Task
-from django.views.generic import ListView, DetailView
-
-# Create your views here.
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 def welcome(request):
-    return render(request, 'welcome.html')
+    return render(request, 'welcome.html', {'current_year': datetime.now().year})
 
 def about(request):
-    return render(request, 'about.html')
-
-# Classes for views
+    return render(request, 'about.html', {'current_year': datetime.now().year})
 
 class TaskListView(ListView):
     model = Task
     template_name = "tasks_list.html"
     context_object_name = 'tasks'
-    ordering = ['-date']  # Order by date descending
+    ordering = ['-date']
 
 class TaskDetailView(DetailView):
     model = Task
     template_name = "task_detail.html"
     context_object_name = 'task'
+
+class TaskCreateView(CreateView):
+    model = Task
+    template_name = "task_form.html"
+    fields = ['title', 'description', 'complete']
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        return redirect('tasks_list')
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = "task_form.html"
+    fields = ['title', 'description', 'complete']
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = "task_confirm_delete.html"
+    success_url = reverse_lazy('tasks_list')
